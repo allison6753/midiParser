d79040a6c299c5897a565151c29565b8b23d4669
allison6753 <alee455@gatech.edu>
2021-01-06 18:06:05 -0500

    added client for testing. Changed midiParser.js and findKey.js into functions.

---
+++ client.js
@@ -0,0 +1,9 @@
+var findKey = require('./findKey.js');
+
+var key = findKey.findKey('./TheLazySong.mid');
+
+// var midiParser = require('./midiParser');
+
+// var answer = midiParser.midiParser('./TheLazySong.mid');
+
+console.log(key);
--- midiParser.js
+++ midiParser.js
@@ -1,60 +1,30 @@
 var fs = require('fs')
-var midiParser  = require('./main.js');
+var Main  = require('./main.js');
 
-console.log('Reading ./Thelazysong.mid as base64...')
-fs.readFile('./Thelazysong.mid', 'base64', function (err,data) {
-  if (err) {
-    throw new Error(err);
-  }
-  console.log('Done!');
-  console.log('Converting base64 string to structured Array...')
-  var midiArray = midiParser.parse(data);
-  console.log('Done!');
-  //console.log(midiArray);
+function midiParser(midiFile) {
 
+    //read midi file
+    var data = fs.readFileSync(midiFile, 'base64', function (err,data) {
+        if (err) {
+            throw new Error(err);
+        }
+    });
 
-  var myEvents = midiArray.track[0].event;
-  var notes = new Array(0,0,0,0,0,0,0,0,0,0,0,0);
-  //let notes = new Map();
-  //var totalCount = myEvents.length;
-  myEvents.forEach(function (event) {
-    // console.log(event.type);
-    //var noteName;
-    if (event.type == 9) {
-    
-        // switch(event.data[0] % 12) {
-        //     case 0: noteName = "C";
-        //         break;
-        //     case 1: noteName = "C#";
-        //         break;
-        //     case 2: noteName = "D";
-        //         break;
-        //     case 3: noteName = "D#";
-        //         break;
-        //     case 4: noteName = "E";
-        //         break;
-        //     case 5: noteName = "F";
-        //         break;
-        //     case 6: noteName = "F#";
-        //         break;
-        //     case 7: noteName = "G";
-        //         break;
-        //     case 8: noteName = "G#";
-        //         break;
-        //     case 9: noteName = "A";
-        //         break;
-        //     case 10: noteName = "A#";
-        //         break;
-        //     case 11: noteName = "B";
-        //         break;
-        // }
+    //parse midi file
+    var midiArray = Main.parse(data);
+    var myEvents = midiArray.track[0].event;
+    var notes = new Array(0,0,0,0,0,0,0,0,0,0,0,0);
 
-        var note = event.data[0] % 12;
-        notes[note]++;
-    }
+    //find frequency of each pitch and store in array
+    myEvents.forEach(function (event) {
+        if (event.type == 9) {
+            var note = event.data[0] % 12;
+            notes[note]++;
+        }
+    });
 
-  });
+    return notes;
 
-  console.log(notes);
+}
 
-});
+module.exports = { midiParser }
